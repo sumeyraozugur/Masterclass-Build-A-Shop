b56b1c8f56589ace70235a0fec431732dbeedc90
123Sumeyra <ozugursumeyra@gmail.com>
2022-07-03 18:05:58 +0300

    profile xml done

--- ECommerce/app/src/main/java/com/sum/ecommerce/MainActivity.kt
+++ ECommerce/app/src/main/java/com/sum/ecommerce/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.sum.ecommerce
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.sum.ecommerce.databinding.ActivityMainBinding
+import com.sum.ecommerce.utils.Constants
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // TODO Step 4: Get the stored username from the Android SharedPreferences.
+        // START
+        // Create an instance of Android SharedPreferences
+        val sharedPreferences =
+            getSharedPreferences(Constants.MYSHOPPAL_PREFERENCES, Context.MODE_PRIVATE)
+
+        val username = sharedPreferences.getString(Constants.LOGGED_IN_USERNAME, "")!!
+        // Set the result to the tv_main.
+        binding.tvMain.text= "The logged in user is $username."
     }
 }
--- ECommerce/app/src/main/java/com/sum/ecommerce/firestore/FirestoreClass.kt
+++ ECommerce/app/src/main/java/com/sum/ecommerce/firestore/FirestoreClass.kt
@@ -1,6 +1,8 @@
 package com.sum.ecommerce.firestore
 
 import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -63,6 +65,20 @@ class FirestoreClass {
 
                 // Here we have received the document snapshot which is converted into the User Data model object.
                 val user = document.toObject(User::class.java)!!
+
+                val sharedPreferences =
+                    activity.getSharedPreferences(
+                        Constants.MYSHOPPAL_PREFERENCES,
+                        Context.MODE_PRIVATE
+                    )
+
+                // Create an instance of the editor which is help us to edit the SharedPreference.
+                val editor: SharedPreferences.Editor = sharedPreferences.edit()
+                editor.putString(
+                    Constants.LOGGED_IN_USERNAME,
+                    "${user.firstName} ${user.lastName}"
+                )
+                editor.apply()
 
                 // TODO Step : Pass the result to the Login Activity.
                 // START
